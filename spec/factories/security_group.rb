c10a6b53199729aa662cf1e600c2993829b49aad
Brian Semrad <bjsemrad@gmail.com>
2018-03-20 07:59:31 -0500

    Updating SecurityGroup / Ingress Spec (#27)
    * Updating SecurityGroup / Ingress Spec to allow From/To to be conditional per AWS CFT Specs
    
    * Update cfn_parser_security_group_spec.rb
    
    * Fixing Merge format.
    
    * Update security_group.rb

--- spec/factories/security_group.rb
+++ spec/factories/security_group.rb
@@ -26,6 +26,35 @@ def security_group_with_one_ingress_rule(cfn_model: CfnModel.new, security_group
   expected_security_group
 end
 
+def security_group_with_one_ingress_rule_ipprotocol(cfn_model: CfnModel.new, security_group_id: 'sg3', ingress_group_id: nil)
+  ingress_rule = AWS::EC2::SecurityGroupIngress.new cfn_model
+  ingress_rule.cidrIp = '10.1.2.3/32'
+  ingress_rule.ipProtocol = '-1'
+  ingress_rule.groupId = ingress_group_id
+
+  expected_security_group = AWS::EC2::SecurityGroup.new cfn_model
+  expected_security_group.vpcId = { 'Ref' => 'VpcId' }
+  expected_security_group.groupDescription = 'some_group_desc'
+  expected_security_group.logical_resource_id = security_group_id
+  expected_security_group.ingresses << ingress_rule
+  expected_security_group.securityGroupIngress << {
+      'CidrIp' => '10.1.2.3/32',
+      'IpProtocol' => '-1'
+  }
+
+  yield expected_security_group, ingress_rule if block_given?
+  expected_security_group
+end
+
+def standalone_ingress_rule_ip_protocol(cfn_model: CfnModel.new)
+  expected_ingress_rule = AWS::EC2::SecurityGroupIngress.new cfn_model
+  expected_ingress_rule.cidrIp = '10.1.2.3/32'
+  expected_ingress_rule.ipProtocol = '-1'
+  expected_ingress_rule.groupId = 'group1'
+
+  expected_ingress_rule
+end
+
 def security_group_with_no_rules(cfn_model: CfnModel.new, id: 'sg')
   expected_security_group = AWS::EC2::SecurityGroup.new cfn_model
   expected_security_group.vpcId = { 'Ref' => 'VpcId' }
@@ -145,4 +174,33 @@ def security_group_with_one_ingress_and_one_egress_rule(cfn_model: CfnModel.new,
 
   yield expected_security_group, ingress_rule, egress_rule if block_given?
   expected_security_group
+end
+
+def security_group_with_one_egress_rule_ipprotocol(cfn_model: CfnModel.new, security_group_id: 'sg3', ingress_group_id: nil)
+  egress_rule = AWS::EC2::SecurityGroupEgress.new cfn_model
+  egress_rule.cidrIp = '10.1.2.3/32'
+  egress_rule.ipProtocol = '-1'
+  egress_rule.groupId = ingress_group_id
+
+  expected_security_group = AWS::EC2::SecurityGroup.new cfn_model
+  expected_security_group.vpcId = { 'Ref' => 'VpcId' }
+  expected_security_group.groupDescription = 'some_group_desc'
+  expected_security_group.logical_resource_id = security_group_id
+  expected_security_group.egresses << egress_rule
+  expected_security_group.securityGroupEgress << {
+      'CidrIp' => '10.1.2.3/32',
+      'IpProtocol' => '-1'
+  }
+
+  yield expected_security_group, egress_rule if block_given?
+  expected_security_group
+end
+
+def standalone_egress_rule_ip_protocol(cfn_model: CfnModel.new)
+  expected_egress_rule = AWS::EC2::SecurityGroupEgress.new cfn_model
+  expected_egress_rule.cidrIp = '10.1.2.3/32'
+  expected_egress_rule.ipProtocol = '-1'
+  expected_egress_rule.groupId = 'group1'
+
+  expected_egress_rule
 end
