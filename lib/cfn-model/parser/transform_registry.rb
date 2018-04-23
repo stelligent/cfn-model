c5f9d12956c6262d56c802433588e2116fe8f753
Jeff Bachtel <jeffrey.bachtel@stelligent.com>
2018-04-22 22:21:17 -0400

    Issue #31 lint

--- lib/cfn-model/parser/transform_registry.rb
+++ lib/cfn-model/parser/transform_registry.rb
@@ -2,28 +2,32 @@ Dir["#{__dir__}/../transforms/*.rb"].each do |transform|
   require "cfn-model/transforms/#{File.basename(transform, '.rb')}"
 end
 
-class CfnModel::TransformRegistry
-  attr_reader :registry
+class CfnModel
+  # TransformRegistry provides a registry of CloudFormation transforms
+  # available for templates
+  class TransformRegistry
+    attr_reader :registry
 
-  def initialize
-    @registry = {
-      'AWS::Serverless-2016-10-31' => CfnModel::Transforms::Serverless
-    }
-  end
-
-  def perform_transforms(cfn_hash)
-    transform_name = cfn_hash['Transform']
-    return unless transform_name
-    @registry[transform_name].instance.perform_transform cfn_hash
-  end
-
-  def self.instance
-    if @instance.nil?
-      @instance = TransformRegistry.new
+    def initialize
+      @registry = {
+        'AWS::Serverless-2016-10-31' => CfnModel::Transforms::Serverless
+      }
     end
-    @instance
+
+    def perform_transforms(cfn_hash)
+      transform_name = cfn_hash['Transform']
+      return unless transform_name
+      @registry[transform_name].instance.perform_transform cfn_hash
+    end
+
+    def self.instance
+      @instance ||= TransformRegistry.new
+      @instance
+    end
   end
 end
 
-class CfnModel::Transforms
+class CfnModel
+  class Transforms
+  end
 end
