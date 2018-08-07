9fcb7bf2cfcb2ad4603946f1f880f087be62b841
Fernando J Pando <fernando.pando@stelligent.com>
2018-08-07 13:30:54 -0400

    ImageId no longer required
    With the new AWS::EC2::LaunchTemplate resource, previously required properties in
    AWS::EC2::Instance are no longer required, when LaunchTemplateId is specified.
    
    https://github.com/stelligent/cfn_nag/issues/136
    
    Add Rakefile for running 'rake spec' serverspec tests

--- spec/parser/cfn_parser_ec2_instance_spec.rb
+++ spec/parser/cfn_parser_ec2_instance_spec.rb
@@ -13,12 +13,10 @@ describe CfnParser do
         cfn_model = @cfn_parser.parse IO.read(test_template)
 
         ec2_instances = cfn_model.resources_by_type('AWS::EC2::Instance')
-
         expect(ec2_instances.size).to eq 1
+
         ec2_instance = ec2_instances.first
-
         expected_security_group = security_group_with_one_ingress_and_one_egress_rule
-
         actual_security_group = ec2_instance.security_groups.first
         expect(actual_security_group).to eq expected_security_group
       end
@@ -31,11 +29,26 @@ describe CfnParser do
         cfn_model = @cfn_parser.parse IO.read(test_template)
 
         ec2_instances = cfn_model.resources_by_type('AWS::EC2::Instance')
-
         expect(ec2_instances.size).to eq 1
-        ec2_instance = ec2_instances.first
 
+        ec2_instance = ec2_instances.first
         expect(ec2_instance.security_groups.size).to eq 0
+      end
+    end
+  end
+
+  context 'an ec2 instance with a launch template', :ec2 do
+    it 'returns ec2 instance with launch template' do
+      yaml_test_templates('ec2_instance/instance_with_launch_template').each do |test_template|
+        cfn_model = @cfn_parser.parse IO.read(test_template)
+
+        ec2_launch_templates = cfn_model.resources_by_type('AWS::EC2::LaunchTemplate')
+        expect(ec2_launch_templates.size).to eq 1
+
+        ec2_instances = cfn_model.resources_by_type('AWS::EC2::Instance')
+        expect(ec2_instances.size).to eq 1
+        expect(ec2_instances[0].launchTemplate).not_to be_empty
+        expect(ec2_instances[0].imageId).to be_nil
       end
     end
   end
