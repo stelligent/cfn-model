c30572f768e7a8eeac2a96b180d047df22ec5329
Jesse R. Adams <jesse.adams@stelligent.com>
2019-05-03 08:02:20 -0700

    Support older versions of psych

--- lib/cfn-model/psych/visitors/to_ruby_with_line_numbers.rb
+++ lib/cfn-model/psych/visitors/to_ruby_with_line_numbers.rb
@@ -3,11 +3,12 @@ class ToRubyWithLineNumbers < Psych::Visitors::ToRuby
     o.children.each_slice(2) { |k,v|
       key = accept(k)
       val = accept(v)
+      line = v.respond_to?(:line) ? v.line : v.start_line
 
       # This is the important bit. If the value is a scalar,
       # we replace it with the desired hash.
       if v.is_a?(::Psych::Nodes::Scalar) && key == 'Type'
-        val = { "value" => val, "line" => v.start_line + 1} # line is 0 based, so + 1
+        val = { "value" => val, "line" => line + 1} # line is 0 based, so + 1
       end
 
       hash[key] = val
