f98891cadbcdafb13c6b6ecccc6f1b35fed6c3bd
Patrick Shelby <pshelby@users.noreply.github.com>
2020-02-24 09:32:15 -0500

    #62 Enhancing Serverless transform with Api generated resources (#62)
    * stelligent/cfn_nag#337 Adding 'generated resources' when a SAM template is transformed which contains a ServerlessRestApi reference.
    
    * Correcting syntax of PathMapping resource.
    
    * stelligent/cfn_nag#337 Passing function name through to API creation for proper URI names, and adding a second function to the test template for single API demo purposes.

--- spec/transforms/serverless_spec.rb
+++ spec/transforms/serverless_spec.rb
@@ -120,7 +120,51 @@ describe CfnModel::Transforms::Serverless do
       expect(
         actual_cfn_model.raw_model['Resources']['HelloWorldFunction']['Code']
       ).to be_nil
+    end
+  end
 
+  context 'Template with Serverless function and Api Event' do
+    it 'creates ServerlessRestApi-related resources' do
+      cloudformation_template_yml = yaml_test_template('sam/serverlessrestapi_as_ref')
+      actual_cfn_model = @cfn_parser.parse cloudformation_template_yml
+      path_map = actual_cfn_model.resources.values.find do |resource|
+        resource.logical_resource_id == 'PathMapping'
+      end
+      serverlessrestapi = actual_cfn_model.resources.values.find do |resource|
+        resource.logical_resource_id == 'ServerlessRestApi'
+      end
+      serverlessrestapi_deployment = actual_cfn_model.resources.values.find do |resource|
+        resource.logical_resource_id == 'ServerlessRestApiDeployment'
+      end
+      serverlessrestapi_stage = actual_cfn_model.resources.values.find do |resource|
+        resource.logical_resource_id == 'ServerlessRestApiProdStage'
+      end
+
+      expect(path_map.resource_type).to eq 'AWS::ApiGateway::BasePathMapping'
+      expect(serverlessrestapi).to_not be_nil
+      expect(serverlessrestapi.body['paths']['/mars']).to_not be_nil
+      expect(serverlessrestapi_deployment).to_not be_nil
+      expect(serverlessrestapi_stage).to_not be_nil
+    end
+  end
+
+  context 'Template with Serverless function but no Api Event' do
+    it 'does not create ServerlessRestApi-related resources' do
+      cloudformation_template_yml = yaml_test_template('sam/no_serverlessrestapi')
+      actual_cfn_model = @cfn_parser.parse cloudformation_template_yml
+      serverlessrestapi = actual_cfn_model.resources.values.find do |resource|
+        resource.logical_resource_id == 'ServerlessRestApi'
+      end
+      serverlessrestapi_deployment = actual_cfn_model.resources.values.find do |resource|
+        resource.logical_resource_id == 'ServerlessRestApiDeployment'
+      end
+      serverlessrestapi_stage = actual_cfn_model.resources.values.find do |resource|
+        resource.logical_resource_id == 'ServerlessRestApiProdStage'
+      end
+
+      expect(serverlessrestapi).to be_nil
+      expect(serverlessrestapi_deployment).to be_nil
+      expect(serverlessrestapi_stage).to be_nil
     end
   end
 end
