5ba2abc36bf1e095158cb2c42e44dea01e306f01
erickascic <eric.kascic@stelligent.com>
2020-03-17 12:54:34 -0400

    Decomposition attempt

--- lib/cfn-model/parser/api_gateway_deployment_parser.rb
+++ lib/cfn-model/parser/api_gateway_deployment_parser.rb
@@ -15,26 +15,59 @@ class ApiGatewayDeploymentParser
 
   private
 
-  def attach_usage_plan_to_api_deployment(cfn_model:, deployment:)
-    api_stages = cfn_model.resources_by_type 'AWS::ApiGateway::Stage'
+  def attached_usage_plans(cfn_model)
     usage_plans = cfn_model.resources_by_type 'AWS::ApiGateway::UsagePlan'
-    usage_plans.each do |usage_plan|
-      next if usage_plan.apiStages.nil?
-      usage_plan.apiStages.each do |up_api_stage|
-        if deployment.stageName.nil?
-          api_stages.each do |stage|
-            if References.resolve_resource_id(stage.deploymentId) == deployment.logical_resource_id
-              if References.resolve_resource_id(up_api_stage['Stage']) == stage.logical_resource_id
-                deployment.usage_plan_ids << usage_plan.logical_resource_id
-              end
-            end
-          end
-        else
-          if up_api_stage['Stage'] == deployment.stageName
-            deployment.usage_plan_ids << usage_plan.logical_resource_id
-          end
-        end
+    usage_plans.select { |usage_plan| !usage_plan.apiStages.nil? }
+  end
+
+  def stages_for_deployment(cfn_model, deployment)
+    stages = cfn_model.resources_by_type 'AWS::ApiGateway::Stage'
+    stages.select { |stage| stage.deploymentId ? References.resolve_resource_id(stage.deploymentId) == deployment.logical_resource_id : false }
+  end
+
+  def deployment_creates_stage?(deployment)
+    !deployment.stageName.nil?
+  end
+
+  def usage_plans_for_stage_by_resource_id(cfn_model, logical_resource_id)
+    usage_plans = attached_usage_plans(cfn_model)
+    usage_plans.select do |usage_plan|
+      stages = usage_plan.apiStages.select do |up_api_stage|
+        References.resolve_resource_id(up_api_stage['Stage']) == logical_resource_id
       end
+      !stages.empty?
+    end
+  end
+
+  def usage_plans_for_stage_by_stage_name(cfn_model, stage_name)
+    usage_plans = attached_usage_plans(cfn_model)
+    usage_plans.select do |usage_plan|
+      stages = usage_plan.apiStages.select do |up_api_stage|
+        up_api_stage['Stage'] == stage_name
+      end
+      !stages.empty?
+    end
+  end
+
+  def attach_usage_plans_for_deployment_that_creates_stage(cfn_model, deployment)
+    usage_plans_for_stage_by_stage_name(cfn_model, deployment.stageName).each do |usage_plan|
+      deployment.usage_plan_ids << usage_plan.logical_resource_id
+    end
+  end
+
+  def attach_usage_plans_for_deployment_without_stage_name(cfn_model, deployment)
+    stages_for_deployment(cfn_model, deployment).each do |stage|
+      usage_plans_for_stage_by_resource_id(cfn_model, stage.logical_resource_id).each do |usage_plan|
+        deployment.usage_plan_ids << usage_plan.logical_resource_id
+      end
+    end
+  end
+
+  def attach_usage_plan_to_api_deployment(cfn_model:, deployment:)
+    if deployment_creates_stage?(deployment)
+      attach_usage_plans_for_deployment_that_creates_stage(cfn_model, deployment)
+    else
+      attach_usage_plans_for_deployment_without_stage_name(cfn_model, deployment)
     end
   end
 end
