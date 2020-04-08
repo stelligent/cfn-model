313939bc7c45979bcbf5dfe7de17c7761e7892b4
Stephen Goncher <stephen.goncher@stelligent.com>
2020-04-08 10:38:18 -0400

    #409 Clean up NACLEntry model

--- lib/cfn-model/model/ec2_network_acl_entry.rb
+++ lib/cfn-model/model/ec2_network_acl_entry.rb
@@ -5,8 +5,6 @@ require_relative 'model_element'
 class AWS::EC2::NetworkAclEntry < ModelElement
   def initialize(cfn_model)
     super
-    @icmp = []
-    @portRange = []
     @resource_type = 'AWS::EC2::NetworkAclEntry'
   end
 end
