238b973e175686f35fd60a89b8754d8083ea8293
Bennie Mosher <benniemosher@gmail.com>
2021-10-25 09:31:38 -0600

    Add specs

--- spec/parser/cfn_parser_line_numbers_spec.rb
+++ spec/parser/cfn_parser_line_numbers_spec.rb
@@ -11,10 +11,11 @@ describe CfnParser do
       cloudformation_template_yml = IO.read(yaml_test_templates('iam_user/iam_user_with_literal_username_and_addition').first)
       actual_cfn_model = @cfn_parser.parse cloudformation_template_yml, nil, true
       expected_line_numbers = {
-        "iamUserWithAddition" => 4,
-        "groupA" => 9,
-        "addition1" => 14,
-        "addition2" => 24
+        "AccessKey" => 4,
+        "iamUserWithAddition" => 8,
+        "groupA" => 13,
+        "addition1" => 18,
+        "addition2" => 28
       }
       expect(actual_cfn_model.line_numbers).to eq expected_line_numbers
     end
